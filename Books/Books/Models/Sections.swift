dc902034603c1893b5b40ecca8434e80c71d57c5
Matthew Andrea D'Alessio <jckmatthew99@gmail.com>
2023-11-15 01:11:07 +0100

    I created the list

---
+++ Books/Books/Models/Sections.swift
@@ -0,0 +1,18 @@
+//
+//  Sections.swift
+//  Books
+//
+//  Created by Matthew Andrea D'Alessio on 15/11/23.
+//
+
+import SwiftUI
+
+struct Sections: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Sections()
+}
---
+++ Books/Books/Models/ViewModels/SectionsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SectionsViewModel.swift
+//  Books
+//
+//  Created by Matthew Andrea D'Alessio on 15/11/23.
+//
+
+import SwiftUI
+
+struct SectionsViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SectionsViewModel()
+}
