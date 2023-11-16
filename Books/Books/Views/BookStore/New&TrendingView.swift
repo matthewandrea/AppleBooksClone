0154fe2274f6bb0892da50c1bb627e5e5989ee9b
Matthew Andrea D'Alessio <jckmatthew99@gmail.com>
2023-11-16 11:18:09 +0100

    I fixed some bags

---
+++ Books/Books/Views/BookStore/New&TrendingView.swift
@@ -0,0 +1,18 @@
+//
+//  New&TrendingView.swift
+//  Books
+//
+//  Created by Matthew Andrea D'Alessio on 16/11/23.
+//
+
+import SwiftUI
+
+struct New_TrendingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    New_TrendingView()
+}
