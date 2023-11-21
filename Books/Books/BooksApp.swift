438090a8e6849ef9fc279626a44ef1dabe1e19a3
Matthew Andrea D'Alessio <jckmatthew99@gmail.com>
2023-11-21 13:01:18 +0100

    .

--- Books/Books/BooksApp.swift
+++ Books/Books/BooksApp.swift
@@ -10,35 +10,45 @@ import SwiftUI
 @main
 struct BooksApp: App {
     
+    @State private var selectedTab = 2
+    
     var body: some Scene {
         WindowGroup {
             
-            TabView {
+            TabView(selection: $selectedTab) {
                 ReadNowView()
                     .tabItem {
                         Image(systemName: "book.fill")
                         Text("Read Now")
                     }
+                    .tag(0)
+                
                 LibraryView()
                     .tabItem {
                         Image(systemName: "books.vertical.fill")
                         Text("Library")
                     }
+                    .tag(1)
+                
                 BookStoreView()
                     .tabItem {
                         Image(systemName: "bag.fill")
                         Text("Book Store")
                     }
+                    .tag(2)
                 AudiobooksView()
                     .tabItem {
                         Image(systemName: "headphones")
                         Text("Audiobooks")
                     }
+                    .tag(3)
+                
                 SearchView()
                     .tabItem {
                         Image(systemName: "magnifyingglass")
                         Text("Search")
                     }
+                    .tag(4)
             }
             
         }
